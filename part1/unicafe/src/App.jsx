7d7ec6f99845fcb438b5a9191d8eda5fe35af7eb
DominionSpy <dominionspy@hotmail.com>
2026-02-10 13:58:30 +0200

    Exercise 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,6 +6,21 @@ const Button = (props) => (
     </button>
 )
 
+const Statistics = ({good, neutral, bad}) => {
+    const total = good + neutral + bad
+
+    return (
+        <p>
+            good {good}<br />
+            neutral {neutral}<br />
+            bad {bad}<br />
+            all {total}<br />
+            average {(good - bad) / total}<br />
+            positive {(good * 100) / total}%
+        </p>
+    )
+}
+
 const App = () => {
     // save clicks of each button to its own state
     const [good, setGood] = useState(0)
@@ -19,14 +34,7 @@ const App = () => {
             <Button onClick={() => setNeutral(neutral + 1)} text='neutral' />
             <Button onClick={() => setBad(bad + 1)} text='bad' />
             <h1>statistics</h1>
-            <p>
-                good {good}<br />
-                neutral {neutral}<br />
-                bad {bad}<br />
-                all {(good + neutral + bad)}<br />
-                average {(good - bad) / (good + neutral + bad)}<br />
-                positive {(good * 100) / (good + neutral + bad)}%
-            </p>
+            <Statistics good={good} neutral={neutral} bad={bad} />
         </div>
     )
 }
