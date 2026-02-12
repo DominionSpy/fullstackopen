0a065e4f0be10f1dd8989fba8213f248bc450ce7
DominionSpy <dominionspy@hotmail.com>
2026-02-12 12:39:03 +0200

    Exercise 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      number: '040-1234567'
-    }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [nameFilter, setNameFilter] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -35,10 +36,25 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    setNameFilter(event.target.value)
+  }
+
+  const personsToShow = nameFilter === ''
+    ? persons
+    : persons.filter(person =>
+      person.name.toLowerCase().includes(nameFilter.toLowerCase())
+    )
+
   return (
     <div>
       <div>debug: {newName}</div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with
+        <input value={nameFilter} onChange={handleFilterChange} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name:
@@ -53,8 +69,8 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person =>
-        <span key={person.name}>{person.name} {person.number}<br /></span>
+      {personsToShow.map(person =>
+        <span key={person.id}>{person.name} {person.number}<br /></span>
       )}
     </div>
   )
