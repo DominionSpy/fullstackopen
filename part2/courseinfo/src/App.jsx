2fb1729d622f682275e2c8c445ce034b2dbcbea0
DominionSpy <dominionspy@hotmail.com>
2026-02-12 10:54:28 +0200

    Exercises 2.2-2.3

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -14,16 +14,18 @@ const Part = (props) => (
   </p>
 )
 
+const Total = ({ total }) => <b>total of {total} exercises</b>
+
 const Course = ({ course }) => {
+  const total = course.parts.reduce((sum, part) => sum + part.exercises, 0)
   return (
     <>
       <Header course={course.name}/>
       <Content parts={course.parts}/>
+      <Total total={total}/>
     </>
   )
 }
-
-const Total = (props) => <p>Number of exercises {props.total}</p>
 
 const App = () => {
   const course = {
