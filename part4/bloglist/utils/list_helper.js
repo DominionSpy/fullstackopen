918a98834b9583c72091b2604e38c3a77c9a6be6
DominionSpy <dominionspy@hotmail.com>
2026-02-17 13:04:26 +0200

    Exercises 4.4-4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,19 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((total, blog) => total + blog.likes, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  const likes = blogs.map(blog => blog.likes)
+  const index = likes.indexOf(Math.max(...likes))
+  if (index >= 0) {
+    return blogs[index]
+  }
+  return null
+}
+
 module.exports = {
-  dummy
+  dummy, totalLikes, favoriteBlog
 }
