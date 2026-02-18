04aef39908860bda39f9935491a50345ad037096
DominionSpy <dominionspy@hotmail.com>
2026-02-18 13:17:26 +0200

    Exercise 4.13

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -1,158 +1,149 @@
 const assert = require('node:assert')
-const { test, after, beforeEach } = require('node:test')
+const { test, after, beforeEach, describe } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const helper = require('./test_helper')
 const Blog = require('../models/blog')
 
 const api = supertest(app)
 
-const initialBlogs = [
-  {
-    _id: "5a422a851b54a676234d17f7",
-    title: "React patterns",
-    author: "Michael Chan",
-    url: "https://reactpatterns.com/",
-    likes: 7,
-    __v: 0
-  },
-  {
-    _id: "5a422aa71b54a676234d17f8",
-    title: "Go To Statement Considered Harmful",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-    likes: 5,
-    __v: 0
-  },
-  {
-    _id: "5a422b3a1b54a676234d17f9",
-    title: "Canonical string reduction",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-    likes: 12,
-    __v: 0
-  },
-  {
-    _id: "5a422b891b54a676234d17fa",
-    title: "First class tests",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-    likes: 10,
-    __v: 0
-  },
-  {
-    _id: "5a422ba71b54a676234d17fb",
-    title: "TDD harms architecture",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-    likes: 0,
-    __v: 0
-  },
-  {
-    _id: "5a422bc61b54a676234d17fc",
-    title: "Type wars",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-    likes: 2,
-    __v: 0
-  }
-]
+describe('when there are initially some blogs saved', () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+    await Blog.insertMany(helper.multipleBlogs)
+  })
 
-beforeEach(async () => {
-  await Blog.deleteMany({})
-  await Blog.insertMany(initialBlogs)
-})
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
+  test('unique identifier is named id', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(res => {
+        if (!res.body[0].id) {
+          throw new Error('id is required')
+        }
+      })
+  })
 
-test('unique identifier is named id', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(res => {
-      if (!res.body[0].id) {
-        throw new Error('id is required')
+  describe('addition of a new blog', () => {
+    test('succeeds with valid data', async () => {
+      const newBlog = {
+        title: "Le Fin Est Ici",
+        author: "Matthew Wilson",
+        url: "https://le-fin-est-ici.blogspot.com/",
+        likes: 5,
       }
+
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+      await api
+        .get('/api/blogs')
+        .expect(res => {
+          if (res.body.length !== helper.multipleBlogs.length + 1) {
+            throw new Error('blog not added')
+          }
+        })
+        .expect(/Le Fin Est Ici/)
     })
-})
 
-test('a valid blog can be added', async () => {
-  const newBlog = {
-    title: "Le Fin Est Ici",
-    author: "Matthew Wilson",
-    url: "https://le-fin-est-ici.blogspot.com/",
-    likes: 5,
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-
-  await api
-    .get('/api/blogs')
-    .expect(res => {
-      if (res.body.length !== initialBlogs.length + 1) {
-        throw new Error('blog not added')
+    test('with no likes defaults to zero', async () => {
+      const newBlog = {
+        title: "Le Fin Est Ici",
+        author: "Matthew Wilson",
+        url: "https://le-fin-est-ici.blogspot.com/",
       }
+
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+      await api
+        .get('/api/blogs')
+        .expect(res => {
+          const blog = res.body.find(blog => blog.author === newBlog.author)
+          if (blog.likes !== 0) {
+            throw new Error('blog likes should be zero')
+          }
+        })
     })
-    .expect(/Le Fin Est Ici/)
-})
 
-test('blog added with no likes defaults to zero', async () => {
-  const newBlog = {
-    title: "Le Fin Est Ici",
-    author: "Matthew Wilson",
-    url: "https://le-fin-est-ici.blogspot.com/",
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-
-  await api
-    .get('/api/blogs')
-    .expect(res => {
-      const blog = res.body.find(blog => blog.author === newBlog.author)
-      if (blog.likes !== 0) {
-        throw new Error('blog likes should be zero')
+    test('with no title fails with status code 400', async () => {
+      const newBlog = {
+        author: "Matthew Wilson",
+        url: "https://le-fin-est-ici.blogspot.com/",
+        likes: 5,
       }
+
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400)
+        .expect(/Path `title` is required/)
     })
-})
 
-test('blog added with no title returns an error', async () => {
-  const newBlog = {
-    author: "Matthew Wilson",
-    url: "https://le-fin-est-ici.blogspot.com/",
-    likes: 5,
-  }
+    test.only('with no url fails with status code 400', async () => {
+      const newBlog = {
+        title: "Le Fin Est Ici",
+        author: "Matthew Wilson",
+        likes: 5,
+      }
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
-    .expect(/Path `title` is required/)
-})
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400)
+        .expect(/Path `url` is required/)
+    })
+  })
 
-test.only('blog added with no url returns an error', async () => {
-  const newBlog = {
-    title: "Le Fin Est Ici",
-    author: "Matthew Wilson",
-    likes: 5,
-  }
+  describe('deletion of a blog', () => {
+    test('succeeds with status code 204 if id is valid', async () => {
+      const blogsAtStart = await helper.blogsInDb()
+      const blogToDelete = blogsAtStart[0]
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
-    .expect(/Path `url` is required/)
+      await api
+        .delete(`/api/blogs/${blogToDelete.id}`)
+        .expect(204)
+
+      const blogsAtEnd = await helper.blogsInDb()
+      const ids = blogsAtEnd.map(n => n.id)
+      assert(!ids.includes(blogToDelete.id))
+      assert.strictEqual(blogsAtEnd.length, helper.multipleBlogs.length - 1)
+    })
+
+    test('succeeds with status code 204 if blog does not exist', async () => {
+      const validNonexistingId = await helper.nonExistingId()
+
+      await api
+        .delete(`/api/blogs/${validNonexistingId}`)
+        .expect(204)
+
+      const blogsAtEnd = await helper.blogsInDb()
+      const ids = blogsAtEnd.map(n => n.id)
+      assert(!ids.includes(validNonexistingId))
+      assert.strictEqual(blogsAtEnd.length, helper.multipleBlogs.length)
+    })
+
+    test('fails with status code 400 if id is invalid', async () => {
+      const invalidId = '5a3d5da59070081a82a3445'
+
+      await api
+        .delete(`/api/blogs/${invalidId}`)
+        .expect(400)
+    })
+  })
 })
 
 after(async () => {
