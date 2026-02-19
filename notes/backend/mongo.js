d06a7de65a20be202ae4109f7f372c0698c1582c
DominionSpy <dominionspy@hotmail.com>
2026-02-19 10:12:44 +0200

    Add users and login

--- notes/backend/mongo.js
+++ notes/backend/mongo.js
@@ -7,28 +7,47 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2]
 
-const url = `mongodb+srv://dominionspy_db_user:${password}@cluster0.ynesecc.mongodb.net/noteTest?retryWrites=true&w=majority&appName=Cluster0`
+const url = `mongodb+srv://dominionspy_db_user:${password}@cluster0.ynesecc.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
 
 mongoose.set('strictQuery',false)
 
 mongoose.connect(url, { family: 4 })
 
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
+const userSchema = new mongoose.Schema({
+  username: String,
+  name: String,
+  password: String,
 })
 
-const Note = mongoose.model('Note', noteSchema)
+const User = mongoose.model('User', userSchema)
 
-const note = new Note({
-  content: 'HTML is easy',
-  important: false,
+const user = new User({
+  username: 'mluukkai',
+  name: 'Matti Luukkainen',
+  password: 'salainen',
 })
 
-note.save().then(result => {
-  console.log('note saved!')
+user.save().then(() => {
+  console.log('User saved successfully.')
   mongoose.connection.close()
 })
+
+// const noteSchema = new mongoose.Schema({
+//   content: String,
+//   important: Boolean,
+// })
+//
+// const Note = mongoose.model('Note', noteSchema)
+//
+// const note = new Note({
+//   content: 'HTML is easy',
+//   important: false,
+// })
+//
+// note.save().then(result => {
+//   console.log('note saved!')
+//   mongoose.connection.close()
+// })
 
 // Note.find({}).then(result => {
 //   result.forEach(note => {
