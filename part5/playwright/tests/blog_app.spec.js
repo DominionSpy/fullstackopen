98aacb1e0ba848b61b01056e155a9999c2401f40
DominionSpy <dominionspy@hotmail.com>
2026-02-24 13:38:28 +0200

    Exercise 5.18

--- part5/playwright/tests/blog_app.spec.js
+++ part5/playwright/tests/blog_app.spec.js
@@ -1,7 +1,16 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http://localhost:5173/api/testing/reset')
+    await request.post('http://localhost:5173/api/users', {
+      data: {
+        name: 'Matti Luukkainen',
+        username: 'mluukkai',
+        password: 'salainen'
+      }
+    })
+
     await page.goto('http://localhost:5173')
   })
 
@@ -10,5 +19,30 @@ describe('Blog app', () => {
     await expect(usernameField).toBeVisible()
     const passwordField = page.getByLabel('password')
     await expect(passwordField).toBeVisible()
+  })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await page.getByRole('button', { name: 'login' }).click()
+      await page.getByLabel('username').fill('mluukkai')
+      await page.getByLabel('password').fill('salainen')
+      await page.getByRole('button', { name: 'login' }).click()
+
+      await expect(page.getByRole('button', { name: 'logout' })).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await page.getByRole('button', { name: 'login' }).click()
+      await page.getByLabel('username').fill('mluukkai')
+      await page.getByLabel('password').fill('wrong')
+      await page.getByRole('button', { name: 'login' }).click()
+
+      const errorDiv = page.locator('.error')
+      await expect(errorDiv).toContainText('wrong username or password')
+      await expect(errorDiv).toHaveCSS('border-style', 'solid')
+      await expect(errorDiv).toHaveCSS('color', 'rgb(255, 0, 0)')
+
+      await expect(page.getByRole('button', { name: 'logout' })).not.toBeVisible()
+    })
   })
 })
