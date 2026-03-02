2ca8e32897815c3fc1a016ad38723344fbaf7f43
DominionSpy <dominionspy@hotmail.com>
2026-03-02 13:59:41 +0200

    Exercise 6.16

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
+import anecdoteService from '../services/anecdotes'
 
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
@@ -8,14 +9,15 @@ const anecdoteSlice = createSlice({
       state.push(action.payload)
     },
     addVote(state, action) {
-      console.log(action)
       const id = action.payload
       const anecdoteToChange = state.find(a => a.id === id)
       const changedAnecdote = {
         ...anecdoteToChange,
         votes: anecdoteToChange.votes + 1
       }
-      return state.map(anecdote => (anecdote.id !== id ? anecdote : changedAnecdote))
+      return state.map(anecdote =>
+        anecdote.id !== id ? anecdote : changedAnecdote
+      )
     },
     setAnecdotes(state, action) {
       return action.payload
@@ -23,5 +25,15 @@ const anecdoteSlice = createSlice({
   },
 })
 
-export const { createAnecdote, addVote, setAnecdotes } = anecdoteSlice.actions
+const { setAnecdotes } = anecdoteSlice.actions
+
+export const initializeAnecdotes = () => {
+  return async (dispatch) => {
+    const anecdotes = await anecdoteService.getAll()
+    dispatch(setAnecdotes(anecdotes))
+  }
+}
+
+export const { createAnecdote, addVote } = anecdoteSlice.actions
+
 export default anecdoteSlice.reducer
